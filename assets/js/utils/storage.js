ba7c73434809edb573687f66f5c5dc7bdb139a7a
rfahri <fahrirenngur@outlook.com>
2026-05-22 22:25:41 +0900

    Update storage.js

--- assets/js/utils/storage.js
+++ assets/js/utils/storage.js
@@ -1,1 +1,10 @@
-
+export const StorageUtil = {
+    get(key) {
+        const data = localStorage.getItem(key);
+        return data ? JSON.parse(data) : null;
+    },
+    
+    set(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+};
